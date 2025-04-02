b4872713b8f295c9823660ff6ae9477ed62631bd
dieKrake <kkrazius@gmail.com>
2025-04-02 16:55:58 +0200

    wholescreen animations

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,19 @@ import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
 import IntroSimple from "@/components/intro-simple";
 import { BackgroundBeamsWithCollision } from "@/components/background-beams-with-collision";
+import { BackgroundBeams } from "@/components/background-beams";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      {/* <IntroPicture /> */}
+      {/* <BackgroundBeams className="hidden md:block fixed inset-0 h-screen -z-10" />*/}
+      <BackgroundBeamsWithCollision className="min-h-screen fixed inset-0 -z-10">
+        <></>
+      </BackgroundBeamsWithCollision>
       <div className="flex flex-col items-center px-4 sm:px-0">
-        {/* <Intro /> */}
-        <BackgroundBeamsWithCollision>
-          <IntroSimple />
-        </BackgroundBeamsWithCollision>
-        {/* <SectionDivider /> */}
+        <IntroSimple />
         <About />
         <Projects />
-        {/* <Skills /> */}
         <Contact />
       </div>
     </main>
--- components/background-beams-with-collision.tsx
+++ components/background-beams-with-collision.tsx
@@ -70,7 +70,7 @@ export const BackgroundBeamsWithCollision = ({
     <div
       ref={parentRef}
       className={cn(
-        "h-96 md:h-[40rem] bg-gradient-to-b from-black to-gray-900 relative flex items-center w-full justify-center overflow-hidden",
+        "h-screen bg-gradient-to-b from-black to-gray-900 relative flex items-center w-full justify-center overflow-hidden",
         // h-screen if you want bigger
         className
       )}
